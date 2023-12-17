86b72ded99662dc98f23dc9d469c51824ee83e5b
Genaro Vogelius <genagevo@gmail.com>
2023-12-16 21:06:05 -0300

    sacaste views,serializers,urls

--- power_app/urls.py
+++ power_app/urls.py
@@ -16,22 +16,5 @@ urlpatterns = [
     
     # path('get_users/', views.get_users),
 
-    # API rest
-    # path('admin_usuario/<int:dni>', views.admin_usuario, name="usuario_admin_view"),
-    path('usuario/<int:dni>', views.usuario, name="usuario"),
-    path('tarifas/', views.tarifas, name="tarifas"),
-    path('rutina/<int:dni>/<int:sesion>', views.rutina, name="rutina"),
-    path('sesiones/<int:dni>', views.sesiones, name="sesiones"),
-    path('servicios/', views.servicios, name="servicios"),
-    path('requestRutine/', views.request_rutine, name="request_rutine"),
-    path('carrousel/', views.carrousel, name="carrousel_photos"),
-
-    path('pdf_view/<int:dni>', views.ViewPDF.as_view(), name="pdf_view"),
-
-    path('api/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-
-
     
 ]
