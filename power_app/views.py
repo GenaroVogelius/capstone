74773e3f6580b65f917f5a22e9d630ef4bdaed48
Genaro Vogelius <genagevo@gmail.com>
2023-12-22 12:41:34 -0300

    haciendo un push correcto de prueba

--- power_app/views.py
+++ power_app/views.py
@@ -134,7 +134,8 @@ def sesiones(request, dni):
         sesiones_list = Rutina.objects.filter(usuario__DNI=dni).values_list('sesion', flat=True)
         if not sesiones_list:
             return Response(
-            "mala response"
+            {"not found": f"No tienes ninguna sesion cargada"},
+            status=status.HTTP_400_BAD_REQUEST,
         )
         sesiones_list = list(sesiones_list)
         return Response({"sesiones": sesiones_list}, status=status.HTTP_200_OK)
